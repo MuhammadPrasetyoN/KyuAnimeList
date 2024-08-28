ce335e5d6c7e0c777170a9045fab5a81037130f3
MuhammadPrasetyoN <muhammad.prasetyo.2005356@students.um.ac.id>
2024-08-28 12:31:23 +0700

    update components animelist

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -1,20 +1,30 @@
 import Image from "next/image"
 import Link from "next/link"
 
-const AnimeList = ({title, images, id, rank}) => {
+const AnimeList = ({ api }) => {
     return(
-            <Link href={`/${id}`} className="cursor-pointer">
-                <div className="relative overflow-hidden rounded-md">
-                <Image src={images} alt="..." width={350} height={350} className="object-cover" />
-                {rank && (
-                    <span className="absolute bottom-2 left-2 bg-white text-black text-sm font-semibold px-2 py-1 rounded">
-                        Rank: {rank}
-                    </span>
-                )}
-            </div>
-                <h3 className="font-bold md:text-xl text-md p-4">{title}</h3>
-            </Link>
-
+        <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-2 md:gap-4 px-1 md:px-2 container mx-auto">
+            {api.data.map((anime) => {
+                return(
+                <Link href={`/${anime.mal_id}`} key={anime.mal_id} className="cursor-pointer">
+                    <div className="relative overflow-hidden rounded-md">
+                    <Image src={anime.images.webp.image_url} alt="..." width={350} height={350} className="object-cover" />
+                    {anime.rank && (
+                        <span className="absolute bottom-2 left-2 bg-white text-black text-xs sm:text-sm font-semibold px-1 sm:px-2 py-0.5 sm:py-1 rounded">
+                            Rank: {anime.rank}
+                        </span>
+                    )}
+                    {anime.score && (
+                            <span className="absolute bottom-2 right-2 bg-white text-black text-xs sm:text-sm font-semibold px-1 sm:px-2 py-0.5 sm:py-1 rounded">
+                                Score: {anime.score}
+                            </span>
+                    )}
+                    </div>
+                    <h3 className="font-bold md:text-xl text-md p-4">{anime.title}</h3>
+                </Link>
+                )
+            })}   
+        </div>
     )
 }
 
